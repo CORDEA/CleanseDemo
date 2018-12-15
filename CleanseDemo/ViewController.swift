e9cac380368e5a2d55879549791a9004099f4545
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-15 15:10:43 +0900

    initial commit :)

---
+++ CleanseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanseDemo
+//
+//  Created by Yoshihiro Tanaka on 2018-12-15.
+//  Copyright © 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
