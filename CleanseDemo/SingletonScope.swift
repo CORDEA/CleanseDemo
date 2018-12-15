82001dfe0c595a7399826bee7c217bd25bdae60c
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-15 17:36:06 +0900

    define singleton scope

---
+++ CleanseDemo/SingletonScope.swift
@@ -0,0 +1,12 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-15.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Foundation
+import Cleanse
+
+public struct Singleton: Cleanse.Scope {
+}
+
+public typealias SingletonBinder = Binder<Singleton>
