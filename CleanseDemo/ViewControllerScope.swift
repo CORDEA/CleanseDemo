970f0da562098bb9ca093621a5b8176c7e49a82b
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-16 16:06:19 +0900

    add view controller scope

---
+++ CleanseDemo/ViewControllerScope.swift
@@ -0,0 +1,11 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-16.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import Cleanse
+
+public struct ViewController: Cleanse.Scope {
+}
+
+public typealias ViewControllerBinder = Binder<ViewController>
