c8e9aa4f3a81fe3115442907cb114f1843ef0d32
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-16 20:49:12 +0900

    add main view controller

---
+++ CleanseDemo/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-16.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
