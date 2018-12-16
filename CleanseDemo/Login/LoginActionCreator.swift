a6d7baf487d141e1e6bd22a94978135561ba88e3
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-16 14:01:11 +0900

    implement action creator

---
+++ CleanseDemo/Login/LoginActionCreator.swift
@@ -0,0 +1,16 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-16.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+struct LoginActionCreator {
+    private let action: (LoginAction) -> ()
+
+    init<T: BaseSink>(sink: T) where LoginAction == T.Element {
+        action = sink.dispatch
+    }
+
+    func login(key: String) {
+        action(.login(key: key))
+    }
+}
