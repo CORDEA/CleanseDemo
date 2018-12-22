1e75e5465e362cb2c7baca6ca81d91a1fc403b9a
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-22 12:30:50 +0900

    add navigator

---
+++ CleanseDemo/Login/LoginNavigator.swift
@@ -0,0 +1,17 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-22.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class LoginNavigator {
+    private let navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func navigateToMain() {
+    }
+}
