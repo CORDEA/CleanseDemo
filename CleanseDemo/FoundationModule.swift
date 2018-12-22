df331eda59fc32d3ad6ec18708484e233543d3c6
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-22 17:00:54 +0900

    provide api key

--- CleanseDemo/FoundationModule.swift
+++ CleanseDemo/FoundationModule.swift
@@ -13,11 +13,20 @@ struct FoundationModule: Module {
 }
 
 extension UserDefaults {
+    static let apiKeyDefaultsKey = "APIKey"
+
     struct Module: Cleanse.Module {
         static func configure(binder: SingletonBinder) {
             binder.bind(UserDefaults.self).sharedInScope().to {
                 UserDefaults.standard
             }
+            binder.bind(String.self).tagged(with: APIKey.self).to { (defaults: UserDefaults) in
+                defaults.string(forKey: apiKeyDefaultsKey) ?? ""
+            }
         }
+    }
+
+    struct APIKey: Tag {
+        typealias Element = String
     }
 }
