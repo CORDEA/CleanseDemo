a359fe6a09423610413912a9bf964dc4c63b14b9
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 09:16:59 +0900

    define result

---
+++ CleanseDemo/Main/MainResult.swift
@@ -0,0 +1,9 @@
+//
+// Created by Yoshihiro Tanaka on 2018-12-23.
+// Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
+//
+
+enum MainResult {
+    case success(regions: [Region])
+    case failure
+}
