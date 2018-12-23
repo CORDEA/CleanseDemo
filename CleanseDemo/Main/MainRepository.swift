1f0a35f19306562df374d9b7fcb75a7c2d2b6042
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-23 10:16:07 +0900

    fetch regions

--- CleanseDemo/Main/MainRepository.swift
+++ CleanseDemo/Main/MainRepository.swift
@@ -3,13 +3,43 @@
 // Copyright (c) 2018 Yoshihiro Tanaka. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import Cleanse
+import Alamofire
 
 class MainRepository {
     private let client: APIClient
 
     init(client: APIClient) {
         self.client = client
+    }
+
+    func getRegions() -> Maybe<[Region]> {
+        return Maybe.create { [unowned self] emitter -> Disposable in
+            let request = self.client.getRegions()
+            let disposable = Disposables.create {
+                request.cancel()
+            }
+            request.response {
+                if disposable.isDisposed {
+                    return
+                }
+                guard let data = $0.data else {
+                    emitter(.completed)
+                    return
+                }
+                do {
+                    let regions: [String: Region] = try JSONDecoder().decode([String: Region].self, from: data)
+                    emitter(.success(regions.map { (_, region) in
+                        region
+                    }))
+                } catch {
+                    emitter(.error(error))
+                }
+            }
+            return disposable
+        }
     }
 
     struct Module: Cleanse.Module {
